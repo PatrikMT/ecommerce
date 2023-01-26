24d9334bdb6e010f77a591c254dad74b75cf7bff
PatrikMT <patrik_martins_torres@hotmail.com>
2023-01-26 14:16:50 -0300

    bootstrap e react

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+import ProdutoUnico from './components/ProdutoUnico';
+import Home from './components/Home';
+
+
+function App() {
+  return (
+   
+    <div >
+      
+      
+      
+        
+      
+    </div>
+  );
+}
+
+export default App;
